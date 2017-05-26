f77f2d64c5d6387bd12890b02cbf53fe158fa215
Jason Chua <jasonc1804@gmail.com>
2017-05-26 12:45:47 -0400

    added safety

--- github_post_recieve.php
+++ github_post_recieve.php
@@ -3,7 +3,8 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-  shell_exec( 'cd /home/public_html/ && git reset --hard HEAD && git pull' );
+  /*shell_exec( 'cd /home/public_html/ && git reset --hard HEAD && git pull' );*/
+  shell_exec( 'cd /home/public_html/ && git pull' );
 }
 
 ?>hi
