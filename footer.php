be54682aad74673eaed90bcd9a5cf9627a311815
Jason Chua <jasonc1804@gmail.com>
2017-08-10 01:29:36 -0400

    Initial

---
+++ footer.php
@@ -0,0 +1,16 @@
+    </div><!-- overall -->
+    <div class="footer">
+    <p class="left">CTV - Cooper City High School - Cooper City, FL</p>
+    <p class="right">Website by <a href="alumni.html#17">Jason Chua</a></p>
+    <div class ="bottom" style=""><center><!-- Start of StatCounter Code -->
+      <script type="text/javascript" language="javascript">var sc_project=2428704; var sc_invisible=0; var sc_partition=23; var sc_security="3467593b"; </script>
+      <script type="text/javascript" language="javascript" src="http://www.statcounter.com/counter/counter.js"></script>
+      <noscript>
+        <img src="http://c24.statcounter.com/counter.php?sc_project=2428704&amp;java=0&amp;security=3467593b&amp;invisible=0" alt="web hit counter" border="0"></a>
+      </noscript>
+      <!-- End of StatCounter Code --></center>
+    </div>
+    </div>
+  </div>
+<script type="text/javascript" src="scripts/retina.min.js"></script>
+</body>
