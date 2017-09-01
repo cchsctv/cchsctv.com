ec0605e3141536476d50be7e436aa30c46af0be2
Jason Chua <jasonc1804@gmail.com>
2017-09-01 13:47:29 -0400

    latest_video.php accepts "year" and "special" url inputs

--- latest_video.php
+++ latest_video.php
@@ -2,25 +2,62 @@
 header( 'Content-type: text/xml' );
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 $xml=simplexml_load_file("video.xml") or die("Error: Cannot create object");
-if (!isset($_GET['number_videos'])) {
-  $number_videos = 0;
-} else {
+
+$query  = explode('&', $_SERVER['QUERY_STRING']);
+$params = array();
+
+foreach( $query as $param ){
+  list($name, $value) = explode('=', $param, 2);
+  $params[urldecode($name)][] = urldecode($value);
+}
+
+$number_videos = 1;
+
+if (isset($_GET['year'])) {
+  //$attribute = '[@year="'.$_GET['year'].'"]';
+  $year = implode("\" or @year=\"",$params['year']);
+  $years = '[@year="'.$year.'"]';
+  $number_videos = "na";
+}
+if (isset($_GET['special'])) {
+  $special = implode("\" or @special=\"",$params['special']);
+  $specials = '[@special="'.$special.'"]';
+  $number_videos = "na";
+}
+if (isset($_GET['number_videos'])) {
   $number_videos = $_GET['number_videos'];
-  $number_videos = $number_videos - 1;
 }
+
+$attribute = "";
+if (isset($years    )){$attribute .= $years;}
+if (isset($specials )){$attribute .= $specials;}
+if (empty($attribute)){$attribute  = "na";}
+
 echo '<eps>';
-for ($i = 0; $i <= $number_videos; $i++){
-  echo '<ep year="'.$xml->ep[$i]['year'];
-  $special = $xml->ep[$i]['special'];
-  if (!empty($special)) {
-    echo '" special="'.$special;
-  }
-  echo '">';
-  echo '<title>'.$xml->ep[$i]->title.'</title>';
-  echo '<aired>'.$xml->ep[$i]->aired.'</aired>';
-  echo '<ft>'.$xml->ep[$i]->ft.'</ft>';
-  echo '<video>'.$xml->ep[$i]->video.'</video>';
-  echo '</ep>';
-}
+xml_out($xml,$attribute,$number_videos);
 echo '</eps>';
+
+function xml_out($xml,$attribute,$number_videos) {
+  if ($attribute != "na"){
+    $xml = $xml->xpath('/eps/ep'.$attribute);
+    $count = count($xml);
+  } else {
+    $xml = $xml->xpath('/eps/ep');
+    $count = $number_videos;
+  }
+
+  for ($i = 0; $i <= $count-1; $i++){
+    echo '<ep year="'.$xml[$i]['year'];
+    $special = $xml[$i]['special'];
+    if (!empty($special)) {
+      echo '" special="'.$special;
+    }
+    echo '">';
+    echo '<title>'.$xml[$i]->title.'</title>';
+    echo '<aired>'.$xml[$i]->aired.'</aired>';
+    echo '<ft>'.$xml[$i]->ft.'</ft>';
+    echo '<video>'.$xml[$i]->video.'</video>';
+    echo '</ep>';
+  }
+}
 ?>
