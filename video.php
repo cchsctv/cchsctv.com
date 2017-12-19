ebf9e737cfd6ea3dcc2b5729e6968d775c8d9d96
Jason Chua <jasonc1804@gmail.com>
2017-12-18 19:43:41 -0500

    Added comments video.php

--- video.php
+++ video.php
@@ -82,18 +82,18 @@ td, th {
 }
 @media only screen and (max-width: 720px) {
   .topnav a:not(:nth-child(2)) {
-    display: none;
+	display: none;
   }
   .topnav a:nth-child(2) {
-    display: inherit;
+	display: inherit;
   }
   .yearbox {
-    width: calc(33.33% - 6px);
+	width: calc(33.33% - 6px);
   }
 }
 @media only screen and (max-width: 540px) {
   .yearbox {
-    width: calc(50% - 6px);
+	width: calc(50% - 6px);
   }
 }
 </style>
@@ -105,33 +105,32 @@ require 'topnav.php';
 <link href="https://vjs.zencdn.net/5.19.2/video-js.min.css" rel="stylesheet">
 
 
-        <video controls id="video" class="video-js vjs-16-9 vjs-big-play-centered" preload="none" poster="ctv_images/videoblankl.jpg"
-        data-setup='{"playbackRates": [1, 1.25, 1.5]}' onClick="playpause()">
-          <source id="srcvideo" src="/episodes/ctv400.mp4" >
-        </video>
+		<video controls id="video" class="video-js vjs-16-9 vjs-big-play-centered" preload="none" poster="ctv_images/videoblankl.jpg"
+		data-setup='{"playbackRates": [1, 1.25, 1.5]}' onClick="playpause()">
+		  <source id="srcvideo" src="/episodes/ctv400.mp4" >
+		</video>
 
 <script src="https://vjs.zencdn.net/5.19.2/video.min.js"></script>
 <noscript>Your browser does not support JavaScript!</noscript>
-        <table id=playlist style="margin-top:1rem">
-          <!--
-          <tr>
-            <td href="#" id="ctv400.mp4" onclick=set_video(this.id);>
-            <a class="download" href="/episodes/ctv400.mp4"download>(mp4)</a>
-            CTV #888 - Example Show<span>
-            Aired: October 7th, 1998<br>
-            Featuring: Chua's Birthday, HTML5, and XML!</span>
+		<table id=playlist style="margin-top:1rem">
+		  <!--
+		  <tr>
+			<td href="#" id="ctv400.mp4" onclick=set_video(this.id);>
+			<a class="download" href="/episodes/ctv400.mp4"download>(mp4)</a>
+			CTV #888 - Example Show<span>
+			Aired: October 7th, 1998<br>
+			Featuring: Chua's Birthday, HTML5, and XML!</span>
 
-            </td>
-          </tr>
-        -->
-          </table>
+			</td>
+		  </tr>
+		-->
+		  </table>
 
 <script type="text/javascript">
-
 var active_year = 2018; //CHANGE THIS TO THE CURRENT YEAR
 
 var video = document.getElementById('video');
-video = videojs('video'); //video.js, remove for html5 player
+video = videojs('video'); 
 var table = document.getElementById('playlist');
 var source = document.createElement('source');
 var srcvideo = document.getElementById('srcvideo');
@@ -141,261 +140,302 @@ var xml_load = false;
 var url_ops = false;
 var year = false;
 var active_video;
+var videoname;
 var epnum;
 var URLParams = {};
 
-//load_xml_doc(active_year).then(function() {
-  get_url_params();
-  videoload = false;
-  autoplay();
-//})
+//Fetch URL Params, if any
+get_url_params();
+//Main Function
+autoplay();
 
+//Logic, Decides what video to play next on video end
 video.on('ended', function() {
-  if(document.getElementsByClassName("hover")[0].parentElement.nextElementSibling){
-    epnum = document.getElementsByClassName("hover")[0].parentElement.nextElementSibling.childNodes[0].id
- } else if (url_ops) {
-    epnum = table.rows[0].cells[0].id;
- } else if (active_video=="117"){
-   window.location.href = "video.php?special=musical&autoplay";
- } else {
-   epnum2file();
-   load_xml_doc(year*1-1);
-   epnum = table.rows[0].cells[0].id;
- }
- set_video(epnum,1);
+	//If the next video exists in the table, set the "epnum" to it
+	if(document.getElementsByClassName("hover")[0].parentElement.nextElementSibling){
+		videoname = document.getElementsByClassName("hover")[0].parentElement.nextElementSibling.childNodes[0].id;
+	} 
+	//If there are URL operators, set "epnum" to 
+	else if (url_ops) {
+		videoname = table.rows[0].cells[0].id;
+	} 
+	//If the last episode is finished, redirect to musicals
+	else if (active_video=="117"){
+		window.location.href = "video.php?special=musical&autoplay";
+	} 
+	//If no other conditions met, go to the previous year
+	else {
+		epnum2file();							//Get the filename, sets "epnum" and "year" of ended video
+		load_xml_doc(year*1-1);					//Go to the previous year
+		videoname = table.rows[0].cells[0].id;		//Set "epnum" the sirst episode in that year
+	}
+	set_video(videoname);		//Set video to "epnum"
 });
 
 function playpause() {
-  //Obsolete as this is the default behaviour in video.js
-  /*
+	//Obsolete as this is the default behaviour in video.js
+	/*
   if (video.paused){
-    video.play();
+	video.play();
   } else {
-    video.pause();
+	video.pause();
   }
   */
 }
 
 function autoplay() {
-  if (URLParams.special){
-    for (i = 0; i < URLParams.special.length; i++){
-      url_ops=URLParams.special.join("\&special=")
-      url_ops="special="+url_ops
-    }
-    xmlhttp.open("GET", "video_fetch.php?"+url_ops+"", true);
-    xmlhttp.send();
-    xml_load = true;
-    return new Promise(function(resolve){
-      xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          xml2table(xmlhttp);
-          table.rows[0].cells[0].onclick()
-          if (URLParams.episode) {
-            epnum=URLParams.episode[0];
-            epnum2file();
-            videoload = false;
-            set_video(epnum);
-          }
-        }
-      };
-    });
-  } else if (URLParams.episode){
-    epnum=URLParams.episode[0];
-    xmlhttp.open("GET", "video_fetch.php?episode2year="+epnum+"", true);
-    xmlhttp.send();
-    xml_load = true;
-    return new Promise(function(resolve){
-      xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          epnum2file();
-          load_xml_doc(year).then(function() {
-            get_url_params();
-            videoload = false;
-            set_video(epnum);
-          })
-        }
-      };
-    });
-  } else if (URLParams.year){
-    load_xml_doc(URLParams.year);
-    if (URLParams.episode) {
-      epnum=URLParams.episode[0];
-      epnum2file();
-      videoload = false;
-      set_video(epnum);
-    }
-  } else {
-    load_xml_doc(active_year);
-  }
-  if (URLParams.autoplay){
-    setTimeout(function() {
-      video.play();
-      videoload = "true";
-    }, 1000);
-  }
+  //Filter for the "sepcial" URL Param
+	if (URLParams.special){
+		//Joins multiple "special" Params into 1 query
+		for (i = 0; i < URLParams.special.length; i++){
+			url_ops=URLParams.special.join("\&special=");
+			url_ops="special="+url_ops
+		}
+		//Assemble query
+		xmlhttp.open("GET", "video_fetch.php?"+url_ops+"", true);
+		xmlhttp.send();		//Send query
+		xml_load = true;
+
+		return new Promise(function(resolve){
+			xmlhttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+					xml2table(xmlhttp);		//Send the response to table
+					table.rows[0].cells[0].onclick();		//Sets the video to be played
+					//If an episode was specified also, set the video to be played
+					if (URLParams.episode) {
+						epnum=URLParams.episode[0];		//Gets the top episode in table
+						epnum2file();
+						videoload = false;
+						set_video(videoname);
+					}
+				}
+			};
+		});
+	} 
+	//If episode was specified in URL Params...
+	else if (URLParams.episode){
+		epnum=URLParams.episode[0];		//Value from the first in array, just in case multiple were specified
+		//Query server to find year of the episode
+		xmlhttp.open("GET", "video_fetch.php?episode2year="+epnum+"", true);
+		xmlhttp.send();
+		xml_load = true;
+		return new Promise(function(resolve){
+			xmlhttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+					epnum2file();
+					load_xml_doc(year).then(function() {
+						get_url_params();
+						videoload = false;
+						set_video(videoname);
+					})
+				}
+			};
+		});
+	} 
+	//If year was specified in URL Params...
+	else if (URLParams.year){
+		load_xml_doc(URLParams.year);
+		//If the episode was also specified in URL Params..
+		if (URLParams.episode) {
+			epnum=URLParams.episode[0];
+			epnum2file();
+			videoload = false;
+			set_video(videoname);
+		}
+	}
+	//If no year or episode was specified in URL Params...
+	else {
+		load_xml_doc(active_year);
+	}
+	//If autoplay was specified in URL Params
+	if (URLParams.autoplay){
+		setTimeout(function() {
+			video.play();
+			videoload = "true";
+		}, 1000);
+	}
 }
 
 
 function set_video(videoname){
-  window.scrollTo(0,0)
-  hover = document.getElementsByClassName("hover")
-  while (hover.length){
-    hover[0].className = hover[0].classList.remove("hover");
-  };
-  vurl = "/episodes/"+videoname;
-  video.src(vurl);
-  video.load();
-  if (videoload === false){
-    videoload = true;
-
-  } else {
-    video.play();
-  }
-  document.getElementById(videoname).classList.add("hover");
-  videoname = document.getElementById(videoname).textContent;
-  videoname = videoname.match(/\d\d\d/i);
-
-  if (url_ops){
-    change_url("","video.php?"+url_ops+"\&episode="+videoname)
-  } else {
-    change_url("","video.php?episode="+videoname)
-  }
-  active_video = videoname
+	window.scrollTo(0,0);		//Scroll to the top
+	hover = document.getElementsByClassName("hover");		//Gets the current "hover" element
+	
+	//Removes the "hover" from the "hover" element
+	while (hover.length){
+		hover[0].className = hover[0].classList.remove("hover");
+	};
+	
+	//Point the player to the video
+	vurl = "/episodes/"+videoname;
+	video.src(vurl);
+	video.load();
+	
+	//Set "videoload" to true, or if already true, play the video
+	if (videoload === false){
+		videoload = true;
+	} else {
+		video.play();
+	}
+	
+	document.getElementById(videoname).classList.add("hover"); //Add "hover" to the currrently playing video
+	
+	//Get the first 3 numbers in the title of the episode and...
+	videoname = document.getElementById(videoname).textContent;
+	videoname = videoname.match(/\d\d\d/i);
+	//Set the corrosponding URL Parameter
+	if (url_ops){
+		//Even if there are existing URL Params
+		change_url("","video.php?"+url_ops+"\&episode="+videoname)
+	} else {
+		change_url("","video.php?episode="+videoname)
+	}
+	active_video = videoname
 }
 
+//Func: It changes the URL.
 function change_url(title, url) {
-  var obj = { Title: title, Url: url };
-  history.replaceState(obj, obj.Title, obj.Url);
+	var obj = { Title: title, Url: url };
+	history.replaceState(obj, obj.Title, obj.Url);
 }
 
+//Func: Searcher that gets the "epnum" and "year" from the "responseXML"
 function epnum2file(){
-  var found = xmlhttp.responseXML.evaluate("/eps/ep[title[contains(.,'"+String(epnum)+"')]]", xmlhttp.responseXML, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
-  getNodes(found).forEach(function (node) {
-    year=node.getAttribute("year");
-    epnum=node.getElementsByTagName("video")[0].childNodes[0].nodeValue;
-  });
-  function getNodes(iterator) {
-    var nodes = [],
-    next = iterator.iterateNext();
-    nodes.push(next);
-    return nodes;
-  }
+	//XPath Expression to select episode
+	var found = xmlhttp.responseXML.evaluate("/eps/ep[title[contains(.,'"+String(epnum)+"')]]", xmlhttp.responseXML, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+	getNodes(found).forEach(function (node) {
+		year=node.getAttribute("year");
+		videoname=node.getElementsByTagName("video")[0].childNodes[0].nodeValue;		//Just in case XPath has multiple results, take the first one
+	});
+	//Counts the length of XPath Result.
+	function getNodes(iterator) {
+		var nodes = [],
+			next = iterator.iterateNext();
+		nodes.push(next);
+		return nodes;
+	}
 }
 
 
 function load_xml_doc(year) {
-  if (isNaN(year)){
-    video.poster("ctv_images/videoblankl.jpg")
-  } else if(year==active_year) {
-    video.poster("ctv_images/videoblankl.jpg")
-  } else {
-    if (xml_load == true) {
-      video.poster('ctv_images/'+String(year).substr(-2)+'arch.jpg')
-    }
-  }
-  return new Promise(function(resolve){
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        xml2table(xmlhttp);
-        resolve("done!");
-        videoload = false;
-        table.rows[0].cells[0].onclick()
-      }
-    };
-    xmlhttp.open("GET", "video_fetch.php?year="+year+"", true);
-    xmlhttp.send();
-    xml_load = true;
-  });
+	if (isNaN(year)){
+		video.poster("ctv_images/videoblankl.jpg")
+	} else if(year==active_year) {
+		video.poster("ctv_images/videoblankl.jpg")
+	} else {
+		if (xml_load == true) {
+			video.poster('ctv_images/'+String(year).substr(-2)+'arch.jpg')
+		}
+	}
+	return new Promise(function(resolve){
+		xmlhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				xml2table(xmlhttp);
+				resolve("done!");
+				videoload = false;
+				table.rows[0].cells[0].onclick()
+			}
+		};
+		xmlhttp.open("GET", "video_fetch.php?year="+year+"", true);
+		xmlhttp.send();
+		xml_load = true;
+	});
 }
 
+//Func: Parses xml.responseXML into the playlist HTML table
 function xml2table(xml) {
-  var i;
-  var xmlDoc = xml.responseXML;
-  var tableTMP="";
-  var x =xmlDoc.getElementsByTagName("ep")
-  for (i = 0; i <x.length; i++) {
-    var video_table = x[i].getElementsByTagName("video")[0].childNodes[0].nodeValue
-    var title_table = x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue
-    var aired_table = x[i].getElementsByTagName("aired")[0].childNodes[0].nodeValue
-    var featu_table = x[i].getElementsByTagName("ft")[0].childNodes[0].nodeValue
+	var i;
+	var xmlDoc = xml.responseXML;
+	var tableTMP="";
+	var x =xmlDoc.getElementsByTagName("ep")
+	//For each *ep* element in the "response.XML", extract the relevent data
+	for (i = 0; i <x.length; i++) {
+		var video_table = x[i].getElementsByTagName("video")[0].childNodes[0].nodeValue
+		var title_table = x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue
+		var aired_table = x[i].getElementsByTagName("aired")[0].childNodes[0].nodeValue
+		var featu_table = x[i].getElementsByTagName("ft")[0].childNodes[0].nodeValue
 
-    tableTMP += "<tr><td href=\"#\"  id=\"" +
-    video_table +
-    "\" onclick=set_video(this.id);><a class=\"download\" href=\"/episodes/" +
-    video_table +
-    "\"download>("+
-    //TODO handle if file has no extention
-    video_table.split('.').pop() +
-    ")</a>"+
-    title_table  +
-    "<span> Aired: " +
-    aired_table +
-    "<br>Featuring: " +
-    featu_table +
-    "</span></td></tr>";
-  }
-  document.getElementById("playlist").innerHTML = tableTMP;
+		//Takes the extracted relevent data and shoves it in "tableTMP"
+		tableTMP += "<tr><td href=\"#\"  id=\"" +
+	video_table +
+	"\" onclick=set_video(this.id);><a class=\"download\" href=\"/episodes/" +
+	video_table +
+	"\"download>("+
+	//TODO handle if file has no extention
+	video_table.split('.').pop() +
+	")</a>"+
+	title_table  +
+	"<span> Aired: " +
+	aired_table +
+	"<br>Featuring: " +
+	featu_table +
+	"</span></td></tr>";
+	}
+	//Set the playlist table equal to the "tableTMP"
+	document.getElementById("playlist").innerHTML = tableTMP;
 }
 
-function get_url_params(){
-  if (location.search) location.search.substr(1).split("&").forEach(function(item) {
-      var s = item.split("="),
-          k = s[0],
-          v = s[1] && decodeURIComponent(s[1]);
-      (URLParams[k] = URLParams[k] || []).push(v)
-  })
+//Func: Turn URL Parameters into an array for JS usage. Ex: video.php?episode=400&autoplay
+function get_url_params(){ 
+	if (location.search) location.search.substr(1).split("&").forEach(function(item) {
+		var s = item.split("="),
+			k = s[0],
+			v = s[1] && decodeURIComponent(s[1]);
+		(URLParams[k] = URLParams[k] || []).push(v)
+	})
 }
 
 
 </script>
+<!--TODO: Fix functionaity in firefox-->
 <div style="margin-top:1rem; display:inline-block;">
   <div class="yearbox">
-    <a href="javascript:load_xml_doc(active_year);">
-    <img src="ctv_images/arch.png" data-rjs="3">
+	<a href="javascript:load_xml_doc(active_year);">
+	<img src="ctv_images/arch.png" data-rjs="3">
   </a></div>
   <div class="yearbox">
-    <a href="javascript:load_xml_doc(2017);">
-    <img src="ctv_images/17arch.jpg" data-rjs="2">
+	<a href="javascript:load_xml_doc(2017);">
+	<img src="ctv_images/17arch.jpg" data-rjs="2">
   </a></div>
   <div class="yearbox">
-    <a href="javascript:load_xml_doc(2016);">
-    <img src="ctv_images/16arch.jpg">
+	<a href="javascript:load_xml_doc(2016);">
+	<img src="ctv_images/16arch.jpg">
   </a></div>
   <div class="yearbox">
-    <a href="javascript:load_xml_doc(2015);">
-    <img src="ctv_images/15arch.jpg">
+	<a href="javascript:load_xml_doc(2015);">
+	<img src="ctv_images/15arch.jpg">
   </a></div>
   <div class="yearbox">
-    <a href="javascript:load_xml_doc(2014);">
-    <img src="ctv_images/14arch-d.jpg">
+	<a href="javascript:load_xml_doc(2014);">
+	<img src="ctv_images/14arch-d.jpg">
   </a></div>
   <div class="yearbox">
-    <a href="javascript:load_xml_doc(2013);">
-    <img src="ctv_images/13arch-d.jpg">
+	<a href="javascript:load_xml_doc(2013);">
+	<img src="ctv_images/13arch-d.jpg">
   </a></div>
   <div class="yearbox">
-    <a href="javascript:load_xml_doc(2012);">
-    <img src="ctv_images/12arch-d.jpg">
+	<a href="javascript:load_xml_doc(2012);">
+	<img src="ctv_images/12arch-d.jpg">
   </a></div>
   <div class="yearbox">
-    <a href="javascript:load_xml_doc(2011);">
-    <img src="ctv_images/11arch-d.jpg">
+	<a href="javascript:load_xml_doc(2011);">
+	<img src="ctv_images/11arch-d.jpg">
   </a></div>
   <div class="yearbox">
-    <a href="javascript:load_xml_doc(2010);">
-    <img src="ctv_images/10arch-d.jpg">
+	<a href="javascript:load_xml_doc(2010);">
+	<img src="ctv_images/10arch-d.jpg">
   </a></div>
   <div class="yearbox">
-    <a href="javascript:load_xml_doc(2009);">
-    <img src="ctv_images/09arch.jpg">
+	<a href="javascript:load_xml_doc(2009);">
+	<img src="ctv_images/09arch.jpg">
   </a></div>
   <div class="yearbox">
-    <a href="javascript:load_xml_doc(2008);">
-    <img src="ctv_images/08arch.jpg">
+	<a href="javascript:load_xml_doc(2008);">
+	<img src="ctv_images/08arch.jpg">
   </a></div>
   <div class="yearbox">
-    <a>
-    <img src="ctv_images/noprior.jpg">
+	<a>
+	<img src="ctv_images/noprior.jpg">
   </a></div>
 </div>
 
