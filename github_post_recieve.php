6f4c48507ce6da704aac365b6f9bdc1db8e1b865
Jason Chua <jasonc1804@gmail.com>
2018-11-28 11:15:03 -0500

    Harder to break github payload handler

--- github_post_recieve.php
+++ github_post_recieve.php
@@ -9,6 +9,11 @@ Contribute at https://github.com/cchsctv/cchsctv.com/
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-  shell_exec( 'cd /home/ctvserver/public_html && git reset --hard HEAD && git pull' ); 
+  //$output = shell_exec( 'cd /home/ctvserver/public_html && git reset --hard HEAD && git pull' ); 
+  $output = shell_exec( 'cd /home/ctvserver/public_html && git fetch --all && git reset --hard origin/master' ); 
+  error_log("$output", 3, "./git_errors");
+  echo "$output";
+} else {
+  echo "hi";
 }
-?>hi
+?>
