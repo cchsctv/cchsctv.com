514167e606f90f02019e6794110615b4950eef63
Jason Chua <jasonc1804@gmail.com>
2018-11-28 22:13:35 -0500

    Fix incomplete merge 6f4c48
    Please check files prior to merging

--- github_post_recieve.php
+++ github_post_recieve.php
@@ -9,21 +9,11 @@ Contribute at https://github.com/cchsctv/cchsctv.com/
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-<<<<<<< HEAD
-  $output = shell_exec( 'cd /home/ctvserver/public_html && git reset --hard HEAD && git pull' ); 
-  error_log($output, 3, "/var/tmp/my-errors.log");
-  echo "$output";
-} else {
-  echo "hi"
-}
-?>
-=======
   //$output = shell_exec( 'cd /home/ctvserver/public_html && git reset --hard HEAD && git pull' ); 
   $output = shell_exec( 'cd /home/ctvserver/public_html && git fetch --all && git reset --hard origin/master' ); 
-  error_log("$output", 3, "./git_errors");
+  error_log("$output", 3, "./git_log");
   echo "$output";
 } else {
   echo "hi";
 }
 ?>
->>>>>>> master
